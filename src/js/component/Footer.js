6e60da750d3bbac43c1a6079c449c84998ba2498
christianmartinroffey <c.martinroffey@gmail.com>
2022-05-25 11:27:23 +0000

    completed formatting for jumbotron section and added copyright sumbol to footer

--- src/js/component/Footer.js
+++ src/js/component/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
 	return (
 		<div className="container-fluid bg-dark d-flex justify-content-center">
 			<div className="m-5 text-white">
-				<p>Copyright Your Website 2022</p>
+				<p>Copyright &copy; Your Website 2022</p>
 			</div>
 		</div>
 	);
--- src/js/component/Jump.js
+++ src/js/component/Jump.js
@@ -2,20 +2,22 @@ import React from "react";
 
 const Jump = () => {
 	return (
-		<div>
-			<div class="m-3">
-				<div class="m-2 bg-light">
-					<div class="m-3">
-						<h1 class="mt-3 fw-light">A Warm Welcome!</h1>
-						<p class="mt-3 mb-2 fw-light">
-							Lorem ipsum dolor sit amet, consectetur adipiscing
-							elit. Vivamus a volutpat nulla. Nunc sed mattis
-							odio. Mauris a nunc turpis.
-						</p>
-						<button class="btn btn-primary mb-5 fw-light">
-							Call to action!
-						</button>
-					</div>
+		<div class="container-fluid mx-auto">
+			<div class="bg-light p-3">
+				<div class="m-3 mt-5">
+					<h1 class="mt-3 fw-light">A Warm Welcome!</h1>
+					<p class="mt-3 mb-2 fw-light">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Vivamus a volutpat nulla. Nunc sed mattis odio. Mauris a
+						nunc turpis. Nullam sodales augue at venenatis
+						venenatis. Duis ac molestie nisi. Maecenas sit amet
+						imperdiet massa, imperdiet facilisis erat. Sed
+						pellentesque, lectus et tincidunt maximus, sapien tortor
+						consectetur ipsum, at vehicula risus mi a libero.
+					</p>
+					<button class="btn btn-primary mb-5 fw-light">
+						<strong>Call to action!</strong>
+					</button>
 				</div>
 			</div>
 		</div>
