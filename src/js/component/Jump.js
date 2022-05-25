0dfbbc5bda8fe0893058feed1f0a7d4c1ba800cb
christianmartinroffey <c.martinroffey@gmail.com>
2022-05-25 11:50:48 +0000

    switched class > className

--- src/js/component/Jump.js
+++ src/js/component/Jump.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Jump = () => {
 	return (
-		<div class="container mx-auto">
-			<div class="bg-light p-3 m-3">
-				<div class="m-3 mt-5">
-					<h1 class="mt-3 fw-light">A Warm Welcome!</h1>
-					<p class="mt-3 mb-2 fw-light">
+		<div className="container mx-auto">
+			<div className="bg-light p-3 m-3">
+				<div className="m-3 mt-5">
+					<h1 className="mt-3 fw-light">A Warm Welcome!</h1>
+					<p className="mt-3 mb-2 fw-light">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 						Vivamus a volutpat nulla. Nunc sed mattis odio. Mauris a
 						nunc turpis. Nullam sodales augue at venenatis
@@ -15,7 +15,7 @@ const Jump = () => {
 						pellentesque, lectus et tincidunt maximus, sapien tortor
 						consectetur ipsum, at vehicula risus mi a libero.
 					</p>
-					<button class="btn btn-primary mb-5 fw-light">
+					<button className="btn btn-primary mb-5 fw-light">
 						<strong>Call to action!</strong>
 					</button>
 				</div>
